e83fb34bcd0c768d170c18b9e3aa46bf02d16956
melisnv <melisnurverir@gmail.com>
2023-01-16 12:03:40 +0100

    solving error now predicts on streamlit

--- website/predict_page.py
+++ website/predict_page.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pickle
 import xgboost as xgb
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 
 
@@ -22,42 +23,51 @@ def load_model():
         return loaded_model
 
 
-xgb_model = load_model()
+#xgb_model = load_model()
+
+
+def read_data(data_path):
+    data = pd.read_csv(data_path)
+
+    return data
+
+
+
+def split_data(data, target_feature):
+    X = data.iloc[:, :-1]
+    y = data[target_feature]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
+
+    return X_train, X_test, y_train, y_test
+
+
+def xgboost_model(X_train, y_train):
+    # make model
+    xgb_model = xgb.XGBClassifier(use_label_encoder=False, n_jobs=-1,eval_metric='mlogloss')
+    xgb_model.fit(X_train, y_train)
+    return xgb_model
+
+
+def model_prediction(xgb_model, X_test):
+    y_pred = xgb_model.predict(X_test)
+    return y_pred
 
 
 def show_predict_page():
     st.title("Client Churn Prediction")
     st.write("""## Fill the information to predict whether client will leave""")
 
-    gender = (
-        'Female',
-        'Male'
-    )
+    gender = ('Female','Male')
 
-    education = (
-        'Uneducated',
-        'Highschool',
-        'College',
-        'Graduate',
-        'Post-graduate',
-        'Doctorate'
-    )
+    education = ('Uneducated', 'High School', 'College', 'Graduate', 'Post-Graduate', 'Doctorate')
 
-    income = (
-        'Less than $40K',
-        '$40K - $60K',
-        '$60K - $80K',
-        '$80K - $120K',
-        '$120K +'
-    )
+    status = ('Single', 'Married', 'Divorced')
+    income = ('Less than $40K', '$40K - $60K', '$80K - $120K', '$60K - $80K', '$120K +')
 
-    status = (
-        'Single',
-        'Married',
-        'Divorced'
-    )
-
-    gen = st.selectbox("Gender",  {"Female": 0, "Male": 1})
+    gen = st.selectbox("Gender",  gender)
+    gen_map = {"Female": 0, "Male": 1}
+    gen = gen_map[gen]
 
     age = st.slider("Age", 18, 100, 25)
     age_dict = {(25, 35): 0, (35, 45): 1, (45, 55): 2, (55, 65): 3, (65, float('inf')): 4}
@@ -83,69 +93,74 @@ def show_predict_page():
             client_history = hist_group
             break
 
-    inc = st.selectbox("Income", {'Less than $40K': 1, '$40K - $60K': 2, '$80K - $120K': 4, '$60K - $80K': 3, '$120K +': 5})
+    credit_limit = st.slider("Credit Limit", int(round(1000.0)), int(round(34516.0)), int(round(2000.0)))
+
+    inc = st.selectbox("Income", income)
+    inc_map = {'Less than $40K': 1, '$40K - $60K': 2, '$80K - $120K': 4, '$60K - $80K': 3, '$120K +': 5}
+    inc = inc_map[inc]
+
     transaction = st.slider("Total Transaction", 0, int(round(18484.000000)), int(round(510.000000)))
 
-    feature_ranges = {
-        "Customer_Age": age,
-        "Gender": gen,
-        "Dependent_count": (1.0, 3),
-        "Education_Level": edu,
-        "Marital_Status": stat,
-        "Income_Category": inc,
-        "Card_Category": (0.0, 3.0),
-        "Months_on_book": client_history,
-        "Total_Relationship_Count": (1, 4),
-        "Months_Inactive_12_mon": inactive,
-        "Contacts_Count_12_mon": (2.0, 4.0),
-        "Credit_Limit": (0.3654273127753303, 3.520352422907489),
-        "Total_Revolving_Bal": (0.895438596491228, 0.8708771929824561),
-        "Avg_Open_To_Buy": (0.4067022086824067, 3.6372370964907135),
-        "Total_Amt_Chng_Q4_Q1": (3.2280701754385968, 11.671052631578949),
-        "Total_Trans_Amt": transaction,
-        "Total_Trans_Ct": (1.5833333333333333, 2.0),
-        "Total_Ct_Chng_Q4_Q1": (2.9745762711864407, 12.76271186440678),
-        "Avg_Utilization_Ratio": (0.3666666666666666, 1.7145833333333331),
-        "Transaction_Count_per_Contact": (1.2777777777777777, 3.87037037037037),
-        "Loyal_Customer": (1.0, 0.0),
-        "Creditworthiness": (0.7455937133620327, 8.887382419041861),
-        "Cr_Util_Rate": (0.3659547645100451, 1.7149103774907906),
+    random_features = {
+        "Dependent_count": (0, 5),
+        "Card_Category": (0, 3),
+        "Total_Relationship_Count": (1, 6),
+        "Contacts_Count_12_mon": (0, 6),
+        "Total_Revolving_Bal": (0, 5000),
+        "Avg_Open_To_Buy": (3, 34516),
+        "Total_Amt_Chng_Q4_Q1": (0, 4),
+        "Total_Trans_Ct": (10, 140),
+        "Total_Ct_Chng_Q4_Q1": (0, 4),
+        "Avg_Utilization_Ratio": (0.0, 0.9),
+        "Transaction_Count_per_Contact": (0.0, 139.0),
+        "Loyal_Customer": (1, 0),
+        "Creditworthiness": (0, 90),
+        "Cr_Util_Rate": (0, 99.9),
     }
 
     allow = st.button("Predict")
 
     if allow:
-        """
-        Try to upload the model and data here and make a fit,train here.
-        then call just like how you did in the jupyter notebook
-        """
-        #X = np.array([gen, age, edu, inactive, stat, client_history, inc, transaction])
-        #X = X.reshape(1, -1)
-        #churn = xgb_model.predict(X.reshape(1, -1))
-        #churn = xgb_model.predict(xgb.DMatrix(X.flatten()))
-        client_data = {}
 
         # generate random values for each feature
-        for feature, range_ in feature_ranges.items():
-            if isinstance(range_, tuple):
-                client_data[feature] = np.random.uniform(range_[0], range_[1])
+        random_client_data = {}
+        for feature, range_ in random_features.items():
+            if range_ is None or len(range_) == 0:
+                random_client_data[feature] = None
             else:
-                client_data[feature] = range_
+                random_client_data[feature] = np.random.choice(range_)
+
+        user_input_features = {"Customer_Age": age, "Gender": gen, "Education_Level": edu,"Marital_Status": stat,
+                               "Income_Category": inc, "Months_on_book": client_history,"Months_Inactive_12_mon": inactive,
+                               "Credit_Limit": credit_limit, "Total_Trans_Amt": transaction}
+        user_input_client_data = {}
+        for feature, value in user_input_features.items():
+            user_input_client_data[feature] = value
 
         # convert the dictionary to a numpy array
+        client_data = {}
+        client_data.update(random_client_data)
+        client_data.update(user_input_client_data)
         client_data = np.array(list(client_data.values()))
+        st.write(client_data)
 
         # reshape the array to 2D
         client_data = client_data.reshape(1, -1)
 
-        client_data_df = pd.DataFrame(client_data, columns=feature_ranges.keys())
+        data = read_data('unscaled_data.csv')
+        X_train, X_test, y_train, y_test = split_data(data, "Attrition_Flag")
 
-        # make the prediction
-        churn = xgb_model.predict(xgb.DMatrix(client_data.reshape(1, -1)))
+        # train model
+        xgb_model = xgboost_model(X_train, y_train)
+        # make prediction
+        y_pred = model_prediction(xgb_model, X_test)
+
+        # make a new client prediction
+        churn = model_prediction(xgb_model, client_data.reshape(1, -1))
 
 
         if churn[0] == 1:
             possibility = "will leave"
         if churn[0] == 0:
             possibility = "will not leave"
-        st.subheader(f"The client is ${possibility} the credit card service.")
+        st.subheader(f"The client is {possibility} the credit card service.")
